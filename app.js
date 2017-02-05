a1574dcfb9bb288634f4a85f2ca0c4a874171c87
Miro Nurmela <miro.nurmela@futurice.com>
2017-02-05 16:52:39 +0200

    Add basic auth for now

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var express = require('express')
-var basicAuth = require('express-basic-auth')
+var auth = require('basic-auth')
 
 var app = express()
 
-app.use(express.static('public'))
+const secretName = 'gt';
+const secretPass = 'nextLevelGT';
 
-app.use(basicAuth({
-    users: { 'gt': 'nextLevelGT' }
-}))
+app.use(function (req, res, next) {
+ let user = auth(req);
+ if (user && user.name === secretName && user.pass === secretPass) {
+   next();
+ }
+ else {
+   res.status(401)
+     .set('WWW-Authenticate', 'Basic realm="GT"')
+     .end();
+ }
+})
+
+app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('App listening')
