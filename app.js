7277bba4f789c663204c4270fae8008653d8e818
Miro Nurmela <miro.nurmela@futurice.com>
2017-02-05 16:32:58 +0200

    Attempt authentivatio

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express')
+var basicAuth = require('express-basic-auth')
+
 var app = express()
 
 app.use(express.static('public'))
+
+app.use(basicAuth({
+    users: { 'gt': 'nextLevelGT' }
+}))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
