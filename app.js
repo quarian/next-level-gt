b33ecfb318bb96a4ef9f19ac3cac85086187c5a4
Miro Nurmela <miro.nurmela@futurice.com>
2017-02-05 16:40:36 +0200

    Port selection for Heroku

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(basicAuth({
     users: { 'gt': 'nextLevelGT' }
 }))
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000')
+app.listen(process.env.PORT || 3000, function () {
+  console.log('App listening')
 })
