db5c2fb7f334c3b970765a08dad51103a5d47618
Miro Nurmela <miro.nurmela@futurice.com>
2017-03-14 21:25:12 +0200

    Remove basic auth for productions sake

--- app.js
+++ app.js
@@ -3,21 +3,6 @@ var auth = require('basic-auth')
 
 var app = express()
 
-const secretName = 'gt';
-const secretPass = 'nextLevelGT';
-
-app.use(function (req, res, next) {
- let user = auth(req);
- if (user && user.name === secretName && user.pass === secretPass) {
-   next();
- }
- else {
-   res.status(401)
-     .set('WWW-Authenticate', 'Basic realm="GT"')
-     .end();
- }
-})
-
 app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3000, function () {
